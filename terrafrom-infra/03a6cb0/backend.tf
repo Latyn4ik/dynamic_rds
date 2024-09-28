9fb4aac1c352ec380e5a296d43b3934a325f685b
Latyn4ik <ilatincev@gmail.com>
2024-09-28 12:12:28 +0300

    upd

--- terrafrom-infra/03a6cb0/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "dynamic_rds/03a6cb0/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "mock"
-  }
-}
--- terrafrom-infra/c60448e/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "dynamic_rds/c60448e/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "mock"
-  }
-}
